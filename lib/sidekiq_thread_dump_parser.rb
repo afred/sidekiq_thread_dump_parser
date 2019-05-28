3bb4cceee2de1c2e71bf6c434377ba98ed6635a6
Andrew Myers <andrew_myers@wgbh.org>
2019-05-28 09:59:19 -0400

    initial commit

---
+++ lib/sidekiq_thread_dump_parser.rb
@@ -0,0 +1,5 @@
+require "sidekiq_thread_dump_parser/version"
+
+module SidekiqThreadDumpParser
+  # Your code goes here...
+end
---
+++ lib/sidekiq_thread_dump_parser/version.rb
@@ -0,0 +1,3 @@
+module SidekiqThreadDumpParser
+  VERSION = "0.1.0"
+end
